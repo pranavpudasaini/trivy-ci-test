24920fb1c2f1d34d9036dfd6aaeb19cf32001da4
pranavpudasaini <23631617+pranavpudasaini@users.noreply.github.com>
2025-12-29 18:58:52 +0545

    fix(ci): encrypt S3 with KMS

--- main.tf
+++ main.tf
@@ -2,6 +2,23 @@ resource "aws_s3_bucket" "test" {
   bucket_prefix = "test-bucket"
 }
 
+resource "aws_kms_key" "s3" {
+  description             = "Custom multi-region KMS key to encrypt S3 bucket"
+  multi_region            = true
+  deletion_window_in_days = 1
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "test" {
+  bucket = aws_s3_bucket.test.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.s3.arn
+    }
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "test" {
   bucket                  = aws_s3_bucket.test.id
   block_public_acls       = false
